c8b4787442eac6e771de261c4faa5b313933022e
silvie74 <s.flidrova@seznam.cz>
2022-02-24 17:19:30 +0100

    1 file modified, 3 added, 1 deleted

---
+++ ukoly/ukol2.js
@@ -0,0 +1,16 @@
+let soucet = 0;
+let cislo = Number(prompt("zadej číslo:"))
+while (cislo!== 0) {
+ 
+soucet = soucet + cislo;
+console.log("Součet je:" + soucet);
+  
+  cislo = Number(prompt("zadej číslo:"));
+  
+  
+}
+
+
+
+
+
